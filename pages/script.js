058a5fe6e191dcdf483ad4c4cbf459e0d44c8fb9
Bayastan <abdimametovbayastan@gmail.com>
2022-11-20 16:19:48 +0300

    first commit

---
+++ pages/script.js
@@ -0,0 +1,209 @@
+let cursor = document.getElementById("cursor");
+let circle = document.querySelector(".cursor-circle");
+
+let setCursorPosition = function(e) {
+  let xPosition = e.clientX - cursor.clientWidth / 2 + "px";
+  let yPosition = e.clientY - cursor.clientHeight / 2 + "px";
+  cursor.style.transform =
+    "translate(" + xPosition + "," + yPosition + ") scale(1)";
+  return {
+    x: xPosition,
+    y: yPosition
+  };
+};
+
+document.addEventListener("mousemove", e => setCursorPosition(e));
+let scaleCursor = function(e, scale) {
+  setCursorPosition(e);
+  cursor.style.transform =
+    "translate(" +
+    setCursorPosition(e).x +
+    "," +
+    setCursorPosition(e).y +
+    ") scale(" +
+    scale +
+    ")";
+};
+
+document.addEventListener("mousedown", function(e) {
+  scaleCursor(e, 0.8);
+  circle.classList.add("animate");
+});
+document.addEventListener("mouseup", function(e) {
+  scaleCursor(e, 1);
+  circle.classList.remove("animate");
+});
+const scrollline = document.querySelector('.scroll-line');
+
+      function fillscrollline(){
+        const windowHeight = window.innerHeight;
+        const fullHeight = document.body.clientHeight;
+        const scrolled = window.scrollY;
+        const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+
+        scrollline.style.width = percentScrolled + '%';
+      };
+
+      window.addEventListener('scroll', fillscrollline);
+      document.addEventListener("DOMContentLoaded", function(event) {
+        document.addEventListener("scroll", function(event) {
+            const animatedBoxes = document.getElementsByClassName("progress-animation");
+            const windowOffsetTop = window.innerHeight + window.scrollY;
+    
+            Array.prototype.forEach.call(animatedBoxes, (animatedBox) => {
+                const animatedBoxOffsetTop = animatedBox.offsetTop;
+    
+                if (windowOffsetTop >= animatedBoxOffsetTop) {
+                    addClass(animatedBox, "fade-in");
+                }
+            });
+        });
+    });
+    
+    function addClass(element, className) {
+        const arrayClasses = element.className.split(" ");
+        if (arrayClasses.indexOf(className) === -1) {
+            element.className += " " + className;
+        }
+    }
+    window.onload = function() {
+      setInterval(function() {
+        var minutes = new Date().getMinutes();
+        document.getElementById("minutes").innerHTML = (minutes < 10 ? '0' : '') + minutes;
+        var hours = new Date().getHours();
+        document.getElementById("hours").innerHTML = (hours < 10 ? '0' : '') + hours;
+      }, 1000);
+    }
+    let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+// Create new audio element
+let curr_track = document.createElement('audio');
+
+// Define the tracks that have to be played
+let track_list = [
+  {
+    name: "Night Owl",
+    artist: "Broke For Free",
+    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
+  },
+  {
+    name: "Enthusiast",
+    artist: "Tours",
+    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
+  },
+  {
+    name: "Shipping Lanes",
+    artist: "Chad Crouch",
+    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
+  },
+];
+
+
+function loadTrack(track_index) {
+  clearInterval(updateTimer);
+  resetValues();
+  curr_track.src = track_list[track_index].path;
+  curr_track.load();
+
+  track_art.style.backgroundImage = "url(" + track_list[track_index].image + ")";
+  track_name.textContent = track_list[track_index].name;
+  track_artist.textContent = track_list[track_index].artist;
+  now_playing.textContent = "PLAYING " + (track_index + 1) + " OF " + track_list.length;
+
+  updateTimer = setInterval(seekUpdate, 1000);
+  curr_track.addEventListener("ended", nextTrack);
+  random_bg_color();
+}
+
+function resetValues() {
+  curr_time.textContent = "00:00";
+  total_duration.textContent = "00:00";
+  seek_slider.value = 0;
+}
+
+// Load the first track in the tracklist
+loadTrack(track_index);
+
+function playpauseTrack() {
+  if (!isPlaying) playTrack();
+  else pauseTrack();
+}
+
+function playTrack() {
+  curr_track.play();
+  isPlaying = true;
+  playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
+
+function pauseTrack() {
+  curr_track.pause();
+  isPlaying = false;
+  playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
+}
+
+function nextTrack() {
+  if (track_index < track_list.length - 1)
+    track_index += 1;
+  else track_index = 0;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function prevTrack() {
+  if (track_index > 0)
+    track_index -= 1;
+  else track_index = track_list.length;
+  loadTrack(track_index);
+  playTrack();
+}
+
+function seekTo() {
+  let seekto = curr_track.duration * (seek_slider.value / 100);
+  curr_track.currentTime = seekto;
+}
+
+function setVolume() {
+  curr_track.volume = volume_slider.value / 100;
+}
+
+function seekUpdate() {
+  let seekPosition = 0;
+
+  if (!isNaN(curr_track.duration)) {
+    seekPosition = curr_track.currentTime * (100 / curr_track.duration);
+
+    seek_slider.value = seekPosition;
+
+    let currentMinutes = Math.floor(curr_track.currentTime / 60);
+    let currentSeconds = Math.floor(curr_track.currentTime - currentMinutes * 60);
+    let durationMinutes = Math.floor(curr_track.duration / 60);
+    let durationSeconds = Math.floor(curr_track.duration - durationMinutes * 60);
+
+    if (currentSeconds < 10) { currentSeconds = "0" + currentSeconds; }
+    if (durationSeconds < 10) { durationSeconds = "0" + durationSeconds; }
+    if (currentMinutes < 10) { currentMinutes = "0" + currentMinutes; }
+    if (durationMinutes < 10) { durationMinutes = "0" + durationMinutes; }
+
+    curr_time.textContent = currentMinutes + ":" + currentSeconds;
+    total_duration.textContent = durationMinutes + ":" + durationSeconds;
+  }
+}
